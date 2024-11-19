b4118d029361a5496fb11221f24c92a176e8bb5d
lachezar-atanasov <lachezar.atanasov99@gmail.com>
2024-11-19 11:52:33 +0200

    Refactoring

--- OfficeAndParkingAPI/EmployeeEndpoints.cs
+++ OfficeAndParkingAPI/EmployeeEndpoints.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OpenApi;
 using OfficeParkingAndBooking.Data;
 using OfficeParkingAndBooking.Data.Models;
-using OfficeAndParkingAPI.DTO;
+using OfficeAndParkingAPI.DTOs;
 namespace OfficeAndParkingAPI;
 
 public static class EmployeeEndpoints
@@ -13,28 +13,33 @@ public static class EmployeeEndpoints
         var group = routes.MapGroup("/api/Employee").WithTags(nameof(Employee));
 
         group.MapGet("/", async (OfficeParkingDbContext db) => await db.Employees
-                .Select(x=>new GetAllEmployeesDTO(x.Firstname,x.Lastname,x.Team!.FullName))
+                .Select(x=>new GetEmployeeDTO(x.Firstname,x.Lastname,x.Team!.FullName))
                 .ToListAsync())
         .WithName("GetAllEmployees")
         .WithOpenApi();
 
-        group.MapGet("/{id}", async Task<Results<Ok<Employee>, NotFound>> (int id, OfficeParkingDbContext db) =>
-        {
-            return await db.Employees.AsNoTracking()
-                .FirstOrDefaultAsync(model => model.Id == id)
-                is Employee model
-                    ? TypedResults.Ok(model)
-                    : TypedResults.NotFound();
-        })
-        .WithName("GetEmployeeById")
-        .WithOpenApi();
+        group.MapGet("/{id}", async Task<Results<Ok<GetEmployeeDTO>, NotFound>> (int id, OfficeParkingDbContext db) =>
+            {
+                var employee = await db.Employees.Include(e => e.Team).AsNoTracking()
+                    .FirstOrDefaultAsync(model => model.Id == id);
+                if (employee == null)
+                {
+                    return TypedResults.NotFound();
+                }
+
+                var model = new GetEmployeeDTO(employee.Firstname, employee.Lastname, employee.Team!.FullName);
+
+                return TypedResults.Ok(model);
+            })
+            .WithOpenApi()
+            .WithName("GetEmployeeById");
 
         group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Employee employee, OfficeParkingDbContext db) =>
         {
             var affected = await db.Employees
                 .Where(model => model.Id == id)
                 .ExecuteUpdateAsync(setters => setters
-                    .SetProperty(m => m.Id, employee.Id)
+                    //.SetProperty(m => m.Id, employee.Id)
                     .SetProperty(m => m.Firstname, employee.Firstname)
                     .SetProperty(m => m.Lastname, employee.Lastname)
                     .SetProperty(m => m.TeamId, employee.TeamId)
