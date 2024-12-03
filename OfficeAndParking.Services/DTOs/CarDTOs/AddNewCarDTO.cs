b3fac62f6820ea34a8c2f48e212ede90d41b71fb
lachezar-work <lachezar.atanasov@leadconsult.eu>
2024-12-03 09:28:05 +0200

    Added validation for registration plate to AddNewCarDTO

--- OfficeAndParking.Services/DTOs/CarDTOs/AddNewCarDTO.cs
+++ OfficeAndParking.Services/DTOs/CarDTOs/AddNewCarDTO.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OfficeAndParkingAPI.Common;
 
 namespace OfficeAndParking.Services.DTOs.CarDTOs
 {
     public class AddNewCarDTO
     {
         public string Brand { get; set; }
+
+        [Range(GlobalConstants.MinCarRegistrationPlateLength, GlobalConstants.MaxCarRegistrationPlateLength, ErrorMessage = "Registration plate length must be between {1} and {2} characters.")]
         public string RegistrationPlate { get; set; }
+
         public string EmployeeId { get; set; }
     }
 }
